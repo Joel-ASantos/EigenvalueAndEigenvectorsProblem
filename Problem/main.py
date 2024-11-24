c10631679c1fc5adde81f997eedb658733bbaa24
Renan Costa <renancs.sousa@gmail.com>
2024-11-24 19:13:51 -0300

    Calculo de determinante

--- Problem/main.py
+++ Problem/main.py
@@ -1,13 +1,24 @@
 import math
 
+# Calculo do Determinante
+def determinante_Matriz(M):
+    if len(M) == 2:
+        det = M[0][0] * M[1][1] - M[0][1] * M[1][0]
+        return det
+    else:
+        det = 0
+        for coluna in range(len(M)):
+            sub_matrix = [linha[:coluna] + linha[coluna+1:] for linha in M[1:]]
+            det += ((-1) ** coluna) * M[0][coluna] * determinante_Matriz(sub_matrix)
+        return det
+    
+    
+    
+    
 # Matriz 4x4
 M = [[2,0,4,2],
     [0,4,8,0],
     [0,0,2,2],
     [0,0,0,6]]
 
-# Calculo do Determinante
-def determinante_Matriz(M):
-    print()
-
-# Main: AutoValor e Autovertor
+print(f"Determinante : {determinante_Matriz(M)}")
